a82501825cfe9a01bdc7f5a6cb20519b8ac89230
kawanoShotaro <56713064+kawanoShotaro@users.noreply.github.com>
2020-12-21 21:48:06 +0900

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src='./js/main.js'></script>
 </head>
 <body>
-    <?php 
-    echo 'hello world';
+   <?php 
+   $pdo = new PDO('mysql:host=s-cdbr-east-02.cleardb.com;dbname=heroku_69d7ae0707fa573;',
+   'b28e163aad61de','f6c8df4'
+    );
+    foreach($pdo->query('select * from article') as $row){
+        echo '<p>';
+        echo $row['id'];
+        echo '</p>';
+    }
     ?>
 </body>
 </html>
